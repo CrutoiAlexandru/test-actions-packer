e5108263578092cf48c89cfd4e5bca6708a6cf59
Crutoi Alexandru <crutoi2001@gmail.com>
2024-01-21 18:27:11 +0200

    create final image

--- setup.ps1
+++ setup.ps1
@@ -4,5 +4,5 @@ git clone https://github.com/CrutoiAlexandru/test-actions-dotnet
 
 cd test-actions-dotnet
 
-C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet build
-C:\Users\Administrator\AppData\Local\Microsoft\dotnet\dotnet run
+dotnet build
+dotnet run
